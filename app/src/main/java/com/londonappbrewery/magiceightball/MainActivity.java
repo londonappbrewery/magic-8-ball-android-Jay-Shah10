d69be2446bf9dcb1da55df3864b1c8915f7d0681
Jay Shah <j24shah@gmail.com>
2018-01-21 15:00:37 -0500

    Finished Project 2. Updated comments.

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -11,6 +11,8 @@ import java.io.Console;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Initializes ImageView and Button.
     private ImageView ballDisplay;
     private Button myButton;
 
@@ -19,24 +21,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Creating ImageView and Button.
         ballDisplay = (ImageView)findViewById(R.id.image_eightball);
         myButton = (Button)findViewById(R.id.askbutton);
 
+        //creates an array of images.
+        //Final, so that it cannot be changed.
         final int[] ballArray = new int[]{R.drawable.ball1, R.drawable.ball2, R.drawable.ball3,
         R.drawable.ball4, R.drawable.ball5};
 
         myButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                // creates an object for random number.
                 Random randomNumber = new Random();
+
+                //variable holds the random number object. Random numbers from 0-4.
                 int number = randomNumber.nextInt(5);
 
+                // Get images from the image array randomly and set that in the ImageView.
                 ballDisplay.setImageResource(ballArray[number]);
 
-            }
-        });
+            }// end of onClick
+        });// end of setOnClickListener.
 
+    }// end of onCreate.
 
-
-    }
-}
+}// end of MainActivity.
